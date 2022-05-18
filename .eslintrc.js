ceb9dcd90db9305ffd9d6f831a849a78032cfa82
lw <1109160884@qq.com>
2022-05-18 14:30:17 +0800

    引入了SVG并抽取了组件，解决了SVG的问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-var-requires': 'off'
   },
   overrides: [
     {
@@ -25,5 +26,8 @@ module.exports = {
         jest: true
       }
     }
-  ]
+  ],
+  "globals":{
+    "__WebpackModuleApi":"writable"
+  }
 }
