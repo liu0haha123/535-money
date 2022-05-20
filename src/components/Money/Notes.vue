6d63f3c3f43dff9f2d3b46fc7d1347970ab04369
lw <1109160884@qq.com>
2022-05-20 08:54:28 +0800

    拆分模块化组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,32 @@
+<template>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" placeholder="在这里输入备注">
+    </label>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Notes'
+};
+</script>
+
+<style lang='scss' scoped>
+.notes{
+  background-color: #f5f5f5;
+  display: flex;
+  font-size: 14px;
+  padding-left:6px;
+  align-items: center;
+  .name{
+    padding-right: 16px;
+  }
+  input{
+    height: 64px;
+    flex-grow: 1;
+    background-color: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,38 @@
+<template>
+    <ul class="types">
+      <li class="selected">支出</li>
+      <li>收入</li>
+    </ul>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Types'
+};
+</script>
+
+<style lang='scss' scoped>
+.types{
+  background-color: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li{
+    width: 50%;
+    display: flex;
+    height: 64px;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    &.selected::after{
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background-color: #333;
+    }
+  }
+}
+</style>
