2500603b5b7cc1cac90d3f2119783a5d2a438ea0
lw <1109160884@qq.com>
2022-05-21 10:56:05 +0800

    优化了数字组件

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,35 +1,78 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{ output }}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button class="ok">OK</button>
-      <button class="zero">0</button>
-      <button>.</button>
+      <button @click="inputContent">1</button>
+      <button @click="inputContent">2</button>
+      <button @click="inputContent">3</button>
+      <button @click="remove">删除</button>
+      <button @click="inputContent">4</button>
+      <button @click="inputContent">5</button>
+      <button @click="inputContent">6</button>
+      <button @click="clear">清空</button>
+      <button @click="inputContent">7</button>
+      <button @click="inputContent">8</button>
+      <button @click="inputContent">9</button>
+      <button @click="ok" :class="ok">OK</button>
+      <button class="zero" @click="inputContent">0</button>
+      <button @click="inputContent">.</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'NumberPad'
-};
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+
+@Component
+export default class NumberPad extends Vue {
+  output = '';
+
+  inputContent(event: MouseEvent) {
+    const button = (event.target as HTMLButtonElement);
+    const input = button.textContent as string;
+    if (this.output.length === 16) {
+      return;
+    }
+    if (this.output === '0') {
+      if ('0123456789'.indexOf(input) >= 0) {
+        this.output = input;
+      } else {
+        this.output += input;
+      }
+      return;
+    }
+    if (this.output.indexOf('.') >= 0 && input === '.') {
+      return;
+    }
+    this.output += input;
+  }
+
+  remove() {
+    if (this.output.length === 1) {
+      this.output = '0';
+    } else {
+      this.output = this.output.slice(0, -1);
+    }
+  }
+
+  clear() {
+    this.output = '0'
+  }
+
+  ok() {
+    console.log("ok");
+  }
+
+}
 </script>
 
 <style lang='scss' scoped>
 @import "~@/assets/style/helper.scss";
-.numberPad{
-  .output{
+
+.numberPad {
+  .output {
+    height: 72px;
     font-size: 36px;
     font-family: Consolas, monospace;
     padding: 9px 16px;
@@ -37,43 +80,56 @@ export default {
     @extend %innerShandow;
     // @extend %clearFix;
   }
-  .buttons{
+
+  .buttons {
     @extend %clearFix;
-    > button{
+
+    > button {
       width: 25%;
       height: 64px;
       background-color: transparent;
       border: none;
-      &:nth-child(1){
+
+      &:nth-child(1) {
         background-color: #f2f2f2;
       }
-      $bg:#f2f2f2;
-      &:nth-child(2){
+
+      $bg: #f2f2f2;
+
+      &:nth-child(2) {
         background-color: $bg;
       }
-      &:nth-child(2), &:nth-child(5){
-        background-color: darken($bg,4%);
+
+      &:nth-child(2), &:nth-child(5) {
+        background-color: darken($bg, 4%);
       }
-      &:nth-child(3), &:nth-child(6), &:nth-child(9){
-        background-color: darken($bg,4*2%);
+
+      &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+        background-color: darken($bg, 4*2%);
       }
-      &:nth-child(4), &:nth-child(7), &:nth-child(10){
-        background-color: darken($bg,4*3%);
+
+      &:nth-child(4), &:nth-child(7), &:nth-child(10) {
+        background-color: darken($bg, 4*3%);
       }
-      &:nth-child(8), &:nth-child(11), &:nth-child(13){
-        background-color: darken($bg,4*4%);
+
+      &:nth-child(8), &:nth-child(11), &:nth-child(13) {
+        background-color: darken($bg, 4*4%);
       }
-      &:nth-child(14){
-        background-color: darken($bg,5*4%);
+
+      &:nth-child(14) {
+        background-color: darken($bg, 5*4%);
       }
-      &:nth-child(12){
-        background-color: darken($bg,6*4%);
+
+      &:nth-child(12) {
+        background-color: darken($bg, 6*4%);
       }
-      &.ok{
+
+      &.ok {
         height: 64*2px;
         float: right;
       }
-      &.zero{
+
+      &.zero {
         width: 50%;
       }
     }
